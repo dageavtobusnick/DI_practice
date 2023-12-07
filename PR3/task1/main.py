07f7d08a550418b66e6c0b5fc9dee9576bdbf7c0
dageavtobusnick <71216640+dageavtobusnick@users.noreply.github.com>
2023-12-07 20:44:02 +0500

    pr3_t1

---
+++ PR3/task1/main.py
@@ -0,0 +1,86 @@
+import os
+import re
+import statistics
+import json
+from bs4 import BeautifulSoup
+
+my_file = os.path.join(os.path.dirname(__file__), os.path.normpath('zip_var_92'))
+result_sort = os.path.join(os.path.dirname(__file__), os.path.normpath('result_sorted.json'))
+result_filtered = os.path.join(os.path.dirname(__file__), os.path.normpath('result_filtered.json'))
+result_charact = os.path.join(os.path.dirname(__file__), os.path.normpath('result_charact.json'))
+
+def file_read(file_name):
+    with open(file_name, encoding='utf-8') as f:
+        str_html = ''
+        lines = f.readlines()
+        for line in lines:
+            str_html += line
+    
+        soup = BeautifulSoup(str_html, 'html.parser')
+
+        item = {}
+        item['city'] = soup.find_all('span', string=re.compile("Город"))[0].get_text().split(':')[1].strip()
+        item['name'] = soup.find_all('h1')[0].get_text().split(':')[1].strip()
+        address = soup.find_all('p')[0].get_text()
+        ind = address.find('Индекс')
+        item["street"] = address[:ind].split(':')[1].strip()
+        item["index"] = int(address[ind:].split(':')[1].strip())
+        item['floor'] = int(soup.find_all('span', attrs={"class": 'floors'})[0].get_text().split(':')[1].strip())
+        item['year'] = int(soup.find_all('span', attrs={"class": 'year'})[0].get_text().split(" в ")[1].strip())
+        item['parking'] = bool(soup.find_all('span', string=re.compile("Парковка"))[0].get_text().split(':')[1].strip())
+        item['img'] = soup.find_all('img')[0]['src']
+        item['rating'] = float(soup.find_all('span', string=re.compile("Рейтинг"))[0].get_text().split(':')[1].strip())
+        item['views'] = int(soup.find_all('span', string=re.compile("Просмотры"))[0].get_text().split(':')[1].strip())
+        
+        return item
+
+
+max_file = 1000
+# max_file = 200
+
+# проходимся по всем файлам
+items = []
+for i in os.listdir(my_file):
+    temp = file_read(os.path.join(my_file, i))
+    items.append(temp)
+
+items = sorted(items, key=lambda x: x['views'], reverse=True)
+
+with open(result_sort, 'w', encoding='utf-8') as f:
+    f.write(json.dumps(items, ensure_ascii=False))
+
+
+filter_views = []
+for olimp in items:
+    if olimp['rating'] >= 4:
+        filter_views.append(olimp)
+
+with open(result_filtered, 'w', encoding='utf-8') as f:
+    f.write(json.dumps(filter_views, ensure_ascii=False))
+
+
+all_views = {}
+cities = {}
+all_views['sum_views'] = 0
+all_views['min_views'] = 10 ** 9 + 1
+all_views['max_views'] = 0
+std_views = [0]
+for olimp in items:
+     temp = olimp['views']
+     all_views['sum_views'] += temp
+     if all_views['min_views'] > temp:
+         all_views['min_views'] = temp
+     if all_views['max_views'] < temp:
+         all_views['max_views'] = temp
+     std_views.append(temp)
+     if olimp['city'] in cities:
+         cities[olimp['city']] += 1
+     else:
+         cities[olimp['city']] = 1
+all_views['avr_views'] = all_views['sum_views'] / len(items)
+all_views['std_views'] = statistics.stdev(std_views)
+
+all_res = [all_views, cities]
+
+with open(result_charact, 'w', encoding='utf-8') as f:
+    f.write(json.dumps(all_res, ensure_ascii=False))
